0ee477c19c56d1cc71dfc3f51e73db70997d0195
Dvshah13 <dvshah13@gmail.com>
2017-04-24 16:36:13 -0500

    adding batch loading to data munging script

--- data_munging_basics-loading-data.py
+++ data_munging_basics-loading-data.py
@@ -38,4 +38,55 @@ import pandas as pd
 # bad_lines_data_df = pd.read_csv('Data Sets for Code/istanbul_data_bad_lines.csv', error_bad_lines = False)
 # print bad_lines_data_df
 
-## Using chunks for big data sets
+## Using chunks for big data sets, this example prints the chunk shape and each chunk
+# df = pd.read_csv('Data Sets for Code/istanbul_market_data.csv', sep=',', decimal='.', chunksize=10)
+# for chunk in df:
+#     print chunk.shape
+#     print chunk
+
+## Using iterator to load big data set
+# df = pd.read_csv('Data Sets for Code/istanbul_market_data.csv', sep=',', decimal='.', iterator = True)
+# print df.get_chunk(10).shape
+# print df.get_chunk(2)
+
+## Using csv packages, DictReader, to iterate small chunks of data from files
+# import csv
+# with open('Data Sets for Code/istanbul_market_data.csv', 'rb') as data_stream:
+#     for n, row in enumerate(csv.DictReader(data_stream, dialect="excel")):
+#         if n == 0:
+#             print n, row
+#         else:
+#             break
+
+## Using csv packages, reader, to iterate small chunks of data from files
+import csv
+# with open('Data Sets for Code/istanbul_market_data.csv', 'rb') as data_stream:
+#     for n, row in enumerate(csv.reader(data_stream, dialect="excel")):
+#         if n == 0:
+#             print row
+#         else:
+#             break
+
+## Using batch parameter to load data
+# filename = 'Data Sets for Code/istanbul_market_data.csv'
+# def batch_read(filename, batch=5):
+#     # open data stream
+#     with open(filename, 'rb') as data_stream:
+#         # reset the batch
+#         batch_output = list()
+#         #iterate over filename
+#         for n, row in enumerate(csv.reader(data_stream, dialect='excel')):
+#             #if the batch is of the right size
+#             if n > 0 and n % batch == 0:
+#                 # yield back the batch as an ndarray
+#                 yield(np.array(batch_output))
+#                 # reset the batch and restart
+#                 batch_output = list()
+#             # otherwise add the row to the batch
+#             batch_output.append(row)
+#         # when the loop is over, yield what's left
+#         yield(np.array(batch_output))
+# import numpy as np
+# for batch_input in batch_read(filename, batch=5):
+#     print batch_input
+#     break
