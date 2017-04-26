b689c02483ec683b47697a2dafe7ac880783b2af
Dvshah13 <dvshah13@gmail.com>
2017-04-26 11:01:35 -0500

    Adding matplotlib functions

--- data_exploratory-data-analysis.py
+++ data_exploratory-data-analysis.py
@@ -7,13 +7,13 @@ import pandas as pd
 df = pd.read_csv('Data Sets for Code/istanbul_market_data.csv', sep=',', decimal='.')
 
 ## Printing head
-# print df.head(5)
+print df.head(5)
 
 ## Now that dataset is loaded, you can get some insight by using the .describe() method, we get the count, mean, standard deviation, min and max values, some percentiles (25, 50 and 75 percent) which gives you a good idea about the distribution of each feature
-# print df.describe()
+print df.describe()
 
 ## You can also visualize the information by using the box plot method
-# df.boxplot()
+df.boxplot()
 
 ## If you need other quantile values, you can use the .quantile() method
 print df.quantile([0.1,0.9]) # the 10 percent and 90 percent values
@@ -22,4 +22,16 @@ print df.quantile([0.1,0.9]) # the 10 percent and 90 percent values
 df.median() # median
 df.feature_name.unique() # type in the feature name you want to examine and you can get information about the levels
 
-## To examine
+## To examine the relation between features, you can create a co-occurence matrix.  Utilize the crosstab method to do this
+# print pd.crosstab(df['FTSE'] > df['FTSE'].mean(), df['DAX'] > df['DAX'].mean()) # results show they occur conjointly quite often, therefore you can suppose a strong relationship between these thwo events
+## You can examine this graphically as well
+import matplotlib.pyplot as plt
+plt.scatter(df['FTSE'], df['DAX'], alpha=1.0, color='k')
+plt.xlabel("FTSE Index")
+plt.ylabel("DAX Index")
+plt.show() # the trend is quite marked; therefore we can deduce that x and y are strongly related
+
+## Checking the distribution of the feature by using a historgram to approximate the distribution
+plt.hist(df['FTSE'], bins=20)
+plt.xlabel("FTSE Index Distribution")
+plt.show() # here we see a fairly normal distribution
